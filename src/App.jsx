337460f8d32882f40e3c180545b08b51d77779ec
radhad-ux <70577224+radhad-ux@users.noreply.github.com>
2021-03-30 20:09:51 +0200

    Buttons created

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,21 @@ export default function App() {
   function decrease(){
     setMyState(myState - 1);
   }
+  function modifyByValue(newValue){
+    setMyState(myState + newValue);
+  }
 
   return (
     <div className="App">
       <h1>React state Exercise</h1>
       <p>state: {myState}</p>
+      <h2>Button without Argument</h2>
       <button onClick={increase}>Increase counter</button>
       <button onClick={decrease}>Decrease counter</button>
+      <hr/>
+      <h2>Button with Argument</h2>
+      <button onClick={() => modifyByValue(2)}>Increase value</button>
+      <button onClick={() => modifyByValue(-3)}>Decrease value</button>
     </div>
   );
 }
